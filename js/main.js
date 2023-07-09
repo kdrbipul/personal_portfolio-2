a9d169621a889ab9fd2cb20fd3f4f359da513164
kdrbipul <kdrbipul@gmail.com>
2023-07-09 16:24:42 +0600

    up to date

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new typed('.text',{
+    strings:["Frontened Developer" , " Youtuber" , "Backend Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
